c1eac672be8a083c0e4efe37200b72e17f7a217f
leandro.amaral <amaral4957@gmail.com>
2023-12-16 17:57:57 -0300

    Create Insert e Count

---
+++ List2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+var lista = new List<string>()
+{
+    "Paulo","Maria","Bia","Pedro","Marcos"
+};
+
+lista.Add("Marta");
+
+lista.Insert(1, "Bia");
+
+Console.WriteLine(lista.Count + " itens");
+
+
+Console.ReadLine();
